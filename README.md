7b5e9ff52ed95088037dfe74921e2375a4273934
WillUK99 <willuk1999@gmail.com>
2021-09-26 16:19:32 +0100

    isPrime memoization function complete -> readme to do

---
+++ README.md
@@ -0,0 +1,6 @@
+# Function memoization
+
+This is a self-memoizing function -> function that can remember previous computed values
+
+## How it works 
+
--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-console.log("hei")
+// isPrime function that will remember its computed values and store prime numbers within its cache
+function isPrime(num) {
+    if (!isPrime.answers) {
+        isPrime.answers = {}
+    }
+
+    if (isPrime.answers[num] !== undefined) {
+        return isPrime.answers[num]
+    }
+
+    var prime = num !== 1 // 1 is not a prime number...
+
+    for (var i = 2; i < num; i++) {
+        if (num % i === 0) {
+            prime = false
+            break
+        }
+    }
+    return isPrime.answers[num] = prime
+}
+
+
+isPrime(3)
+isPrime(6)
+isPrime(97)
+isPrime(66)
+isPrime(37)
+
+console.log(isPrime.answers)
